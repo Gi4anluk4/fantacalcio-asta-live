7f64344c6b626fee708b668e0835dfeefa2b5c98
Gi4anluk4 <catalanogianluca1989@gmail.com>
2026-09-09 00:10:46 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='asta-live-v5-5-0-20260908-1';
+const CACHE='asta-live-v5-5-1-20260908-1';
 const CORE=['./index.html','./style.css','./data.js','./app.js','./cloud.js','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
